484628fe47e7a9e5275543abfb89b050130943fc
BuketEdir <Bktbkt93!>
2022-09-09 13:26:50 +0300

    User page was completed

--- src/service/user.js
+++ src/service/user.js
@@ -1,5 +1,6 @@
 import router from '@/router'
 import store from '@/store/'
+
 //import axios from 'axios'
 
 export default {
@@ -89,10 +90,106 @@ export default {
         var axios = require('axios')
         var config = {
           method: 'delete',
-          url: 'user-api/' + uuid,
+          url: 'user-api/user-api/' + uuid,
+          headers: {},
+        }
+        const response = await axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data))
+            return true
+          })
+          .catch(function (error) {
+            console.log(error)
+            return false
+          })
+        return response
+      } else {
+        router.push({ name: 'Login Admin' })
+      }
+    },
+    async getUsers(state, page) {
+      // CHECK IF USER LOGGED IN ALREADY
+      if (store.getters['auth/checkIfLoggedIn']) {
+        // ROLE CHECK IS NEEDED HERE DUE BY SECURITY
+        var axios = require('axios')
+        var data = JSON.stringify({
+          filters: [],
+          pageNumber: page.page - 1,
+          pageSize: page.rowsPerPage,
+          language: page.language,
+        })
+        var config = {
+          method: 'post',
+          url: 'user-api/get-all-by-filter',
           headers: {
             'Content-Type': 'application/json',
           },
+          data: data,
+        }
+        const response = await axios(config)
+          .then(function (response) {
+            return response.data
+          })
+          .catch(function (error) {
+            console.log(error)
+            return null
+          })
+        return response
+      } else {
+        // ROLE CHECK IS NEEDED HERE
+        router.push({ name: 'Login Admin' })
+      }
+    },
+    async updateUser(state, userData) {
+      if (store.getters['auth/checkIfLoggedIn']) {
+        // ROLE CHECK IS NEEDED HERE DUE BY SECURITY
+        var axios = require('axios')
+        var data = await JSON.parse(
+          JSON.stringify({
+            language: userData.language,
+            userName: userData.userName,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            email: userData.email,
+            password: userData.password,
+            ageRangeEnum: userData.ageRangeEnum,
+            genderEnum: userData.genderEnum,
+          }),
+        )
+        var config = {
+          method: 'put',
+          url: 'user-api/' + userData.uuid,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          data: data,
+        }
+        const response = axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data))
+            return true
+          })
+          .catch(function (error) {
+            console.log(error)
+            return false
+          })
+        return response
+      } else {
+        // ROLE CHECK IS NEEDED HERE
+        router.push({ name: 'Login Admin' })
+      }
+    },
+    async addUser(state, userData) {
+      if (store.getters['auth/checkIfLoggedIn']) {
+        var axios = require('axios')
+        var data = JSON.stringify(userData)
+        var config = {
+          method: 'post',
+          url: 'user-api/',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          data: data,
         }
         const response = await axios(config)
           .then(function (response) {

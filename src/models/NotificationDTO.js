79ee2534cd0e1aa2b52e56fe84d4e461a9fe4443
Yunus Yıldız <yunusyld7@gmail.com>
2022-12-21 14:14:32 +0300

    Notification page is done for now. added gender and age enums into it. ready to set in serevice

--- src/models/NotificationDTO.js
+++ src/models/NotificationDTO.js
@@ -1,10 +1,49 @@
 export default class notificationDTO {
-  constructor(language, title, message, link, base64, send) {
+  static createEmpty() {
+    return new notificationDTO(
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      false,
+      [],
+      [],
+    )
+  }
+  static createFromJson(data) {
+    return new notificationDTO(
+      data.uuid ?? null,
+      data.language ?? null,
+      data.title ?? null,
+      data.message ?? null,
+      data.link ?? null,
+      data.base64 ?? null,
+      data.send ?? null,
+      data.gendersToSend ?? [],
+      data.agesToSend ?? [],
+    )
+  }
+  constructor(
+    uuid,
+    language,
+    title,
+    message,
+    link,
+    base64,
+    send,
+    gendersToSend,
+    agesToSend,
+  ) {
+    this.uuid = uuid
     this.language = language
     this.title = title
     this.message = message
     this.link = link
     this.base64 = base64
     this.send = send
+    this.gendersToSend = gendersToSend
+    this.agesToSend = agesToSend
   }
 }
